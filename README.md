7dc2099f2c5f44103acf902ecb9e1dace66974db
Jason Morris <jason@roundtablelaw.ca>
2020-06-23 21:24:30 -0400

    first working version

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# docassemble-attributes
+# docassemble-DADataType
 
-Testing
+This is a module to give Docassemble the ability to ask datatype-approriate default questions
+with regard to individual variables, so that the "generic object" feature can be used with
+regard to booleans, numbers, etc.
+
+The use case is for rapid prototyping. If you have an external tool with a data structure and known datatypes,
+you can export that datastructure as a set of objects for a docassemble interview, and specify only
+your goal (perhaps a document template), and you will have a bare-bones functional interview without
+having to draft any question blocks.
+
+## Installation
+
+It can be installed inside your docassemble package manager by providing the address for this github repository.
+
+## Use
+
+Inside your interview, load the module and include the default questions interview file.
+
+```
+modules:
+  - docassemble.DADataType
+---
+include:
+  - docassemble.DADataType:/data/questions/DADataType.yml
+```
+
+Now, create an `objects` block, using the available DADT classes, or load objects from a YAML or JSON file.
+
+The available datatypes are currently:
+
+* DADTNumber
+* DADTString
+* DADTBoolean
+* DADTContinue
+* DADTTime
+* DADTDate
+* DADTDateTime
+* DADTEmail
---
+++ docassemble/attributes/DADataType.py
@@ -0,0 +1,143 @@
+# DADataType.py
+
+from docassemble.base.util import DAObject
+
+__all__ = ['DADTBoolean', 'DADTContinue', 'DADTNumber', 'DADTString', 'DADTEmail', 'DADTDate', 'DADTTime', 'DADTDateTime']
+
+class DADataType(DAObject):
+    def init(self, input_type="default", *pargs, **kwargs):
+      self.intput_type = input_type
+      super().init(*pargs, **kwargs)
+    def __str__(self):
+        return str(self.value)
+    def __dir__(self):
+        return dir(self.value)
+    def __contains__(self, item):
+        return self.value.__contains__(item)
+    def __iter__(self):
+        return self.value.__iter__()
+    def __len__(self):
+        return len(self.value)
+    def __reversed__(self):
+        return reversed(self.value)
+    def __getitem__(self, index):
+        return self.value.__getitem__(index)
+    def __repr__(self):
+        return repr(self.value)
+    def __add__(self, other):
+        return self.value.__add__(other)
+    def __sub__(self, other):
+        return self.value.__sub__(other)
+    def __mul__(self, other):
+        return self.value.__mul__(other)
+    def __floordiv__(self, other):
+        return self.value.__floordiv__(other)
+    def __mod__(self, other):
+        return self.value.__mod__(other)
+    def __divmod__(self, other):
+        return self.value.__divmod__(other)
+    def __pow__(self, other):
+        return self.value.__pow__(other)
+    def __lshift__(self, other):
+        return self.value.__lshift__(other)
+    def __rshift__(self, other):
+        return self.value.__rshift__(other)
+    def __and__(self, other):
+        return self.value.__and__(other)
+    def __xor__(self, other):
+        return self.value.__xor__(other)
+    def __or__(self, other):
+        return self.value.__or__(other)
+    def __div__(self, other):
+        return self.value.__div__(other)
+    def __truediv__(self, other):
+        return self.value.__truediv__(other)
+    def __radd__(self, other):
+        return self.value.__radd__(other)
+    def __rsub__(self, other):
+        return self.value.__rsub__(other)
+    def __rmul__(self, other):
+        return self.value.__rmul__(other)
+    def __rdiv__(self, other):
+        return self.value.__rdiv__(other)
+    def __rtruediv__(self, other):
+        return self.value.__rtruediv__(other)
+    def __rfloordiv__(self, other):
+        return self.value.__rfloordiv__(other)
+    def __rmod__(self, other):
+        return self.value.__rmod__(other)
+    def __rdivmod__(self, other):
+        return self.value.__rdivmod__(other)
+    def __rpow__(self, other):
+        return self.value.__rpow__(other)
+    def __rlshift__(self, other):
+        return self.value.__rlshift__(other)
+    def __rrshift__(self, other):
+        return self.value.__rrshift__(other)
+    def __rand__(self, other):
+        return self.value.__rand__(other)
+    def __ror__(self, other):
+        return self.value.__ror__(other)
+    def __neg__(self):
+        return self.value.__neg__()
+    def __pos__(self):
+        return self.value.__pos__()
+    def __abs__(self):
+        return abs(self.value)
+    def __invert__(self):
+        return self.value.__invert__()
+    def __complex__(self):
+        return complex(self.value)
+    def __int__(self):
+        return int(self.value)
+    def __long__(self):
+        return long(self.value)
+    def __float__(self):
+        return float(self.value)
+    def __oct__(self):
+        return self.octal_value
+    def __hex__(self):
+        return hex(self.value)
+    def __index__(self):
+        return self.value.__index__()
+    def __le__(self, other):
+        return self.value.__le__(other)
+    def __ge__(self, other):
+        return self.value.__ge__(other)
+    def __gt__(self, other):
+        return self.value.__gt__(other)
+    def __lt__(self, other):
+        return self.value.__lt__(other)
+    def __eq__(self, other):
+        return self.value.__eq__(other)
+    def __ne__(self, other):
+        return self.value.__ne__(other)
+    def __hash__(self):
+        return hash(self.value)
+    def __bool__(self):
+        return bool(self.value)
+      
+class DADTBoolean(DADataType):
+  #TODO Add validation for input types.
+  pass
+
+class DADTContinue(DADataType):
+  pass
+
+class DADTNumber(DADataType):
+  pass
+
+class DADTString(DADataType):
+  pass
+
+class DADTEmail(DADataType):
+  pass
+
+class DADTDate(DADataType):
+  pass
+
+class DADTTime(DADataType):
+  pass
+
+class DADTDateTime(DADataType):
+  pass
