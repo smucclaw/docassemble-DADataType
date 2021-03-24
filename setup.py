a90779ac019cc6c28cc5e0d3e9fac6e46cbd2e0d
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-03-24 17:01:16 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
                 out.setdefault(package, []).append(prefix+name)
     return out
 
-setup(name='docassemble.attributes',
+setup(name='docassemble.DADataType',
       version='0.0.1',
       description=('A docassemble extension.'),
       long_description='# docassemble-DADataType\r\n\r\nThis is a module to give Docassemble the ability to ask datatype-approriate default questions\r\nwith regard to individual variables, so that the "generic object" feature can be used with\r\nregard to booleans, numbers, etc.\r\n\r\nThe use case is for rapid prototyping. If you have an external tool with a data structure and known datatypes,\r\nyou can export that datastructure as a set of objects for a docassemble interview, and specify only\r\nyour goal (perhaps a document template), and you will have a bare-bones functional interview without\r\nhaving to draft any question blocks.\r\n\r\n## Installation\r\n\r\nIt can be installed inside your docassemble package manager by providing the address for this github repository.\r\n\r\n## Use\r\n\r\nInside your interview, load the module and include the default questions interview file.\r\n\r\n```\r\nmodules:\r\n  - docassemble.DADataType\r\n---\r\ninclude:\r\n  - docassemble.DADataType:/data/questions/DADataType.yml\r\n```\r\n\r\nNow, create an `objects` block, using the available DADT classes, or load objects from a YAML or JSON file.\r\n\r\nThe available datatypes are currently:\r\n\r\n* DADTNumber\r\n* DADTString\r\n* DADTBoolean\r\n* DADTContinue\r\n* DADTTime\r\n* DADTDate\r\n* DADTDateTime\r\n* DADTEmail',
@@ -55,6 +55,6 @@ setup(name='docassemble.attributes',
       namespace_packages=['docassemble'],
       install_requires=[],
       zip_safe=False,
-      package_data=find_package_data(where='docassemble/attributes/', package='docassemble.attributes'),
+      package_data=find_package_data(where='docassemble/DADataType/', package='docassemble.DADataType'),
      )
 
